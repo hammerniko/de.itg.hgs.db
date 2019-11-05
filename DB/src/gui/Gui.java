ffda0f856c1888a331aee7f1d7e8ef2e535beaf9
n.hammer <n.hammer@v212-be.hgssingen.de>
2019-11-05 12:23:46 +0100

    modified

--- DB/src/gui/Gui.java
+++ DB/src/gui/Gui.java
@@ -27,11 +27,11 @@ import javax.swing.JButton;
 import javax.swing.JTabbedPane;
 import a01DBConnection.*;
 
-
 public class Gui extends JFrame {
 
 	private JPanel contentPane;
 	private JMenuItem mntmSpeichernUnter;
+
 	/**
 	 * Launch the application.
 	 */
@@ -52,11 +52,80 @@ public class Gui extends JFrame {
 	 * Create the frame.
 	 */
 	public Gui() {
-		
-		//System Look and feel
+
+		setLookAndFeel();
+
+		// Vollbild
+		setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+
+		setTitle("SSDB 0.1");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 871, 301);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnDatei = new JMenu("Datei");
+		menuBar.add(mnDatei);
+
+		JMenuItem mntmImport = new JMenuItem("DB Importieren");
+		mnDatei.add(mntmImport);
+
+		mntmSpeichernUnter = new JMenuItem("Speichern unter...");
+		mntmSpeichernUnter.setEnabled(false);
+		mntmSpeichernUnter.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clickSaveAs();
+
+			}
+		});
+		mnDatei.add(mntmSpeichernUnter);
+
+		JMenu mnStammdaten = new JMenu("Stammdaten");
+		menuBar.add(mnStammdaten);
+
+		JMenuItem mntmNachlassstunden = new JMenuItem("Nachlassstunden");
+		mnStammdaten.add(mntmNachlassstunden);
+
+		JMenuItem mntmLehrer = new JMenuItem("Lehrer");
+		mnStammdaten.add(mntmLehrer);
+
+		JMenuItem mntmRume = new JMenuItem("RÃ¤ume");
+		mnStammdaten.add(mntmRume);
+
+		JMenuItem mntmKlassen = new JMenuItem("Klassen");
+		mnStammdaten.add(mntmKlassen);
+
+		JMenu mnHilfe = new JMenu("Hilfe");
+		menuBar.add(mnHilfe);
+
+		JMenuItem mntmSkript = new JMenuItem("Skript");
+		mnHilfe.add(mntmSkript);
+
+		mntmImport.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clickImport();
+			}
+		});
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+
+		final JPanel panelStatus = new JPanel();
+		contentPane.add(panelStatus, BorderLayout.SOUTH);
+
+	}
+
+	private void setLookAndFeel() {
+		// System Look and feel
 		try {
-			UIManager.setLookAndFeel(
-			        UIManager.getSystemLookAndFeelClassName());
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -70,99 +139,26 @@ public class Gui extends JFrame {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		
-		
-		//Vollbild
-		setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
-		
-		setTitle("SSDB 0.1");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 871, 301);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu mnDatei = new JMenu("Datei");
-		menuBar.add(mnDatei);
-		
-		JMenuItem mntmImport = new JMenuItem("DB Importieren");
-		mnDatei.add(mntmImport);
-		
-		mntmSpeichernUnter = new JMenuItem("Speichern unter...");
-		mntmSpeichernUnter.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				clickSaveAs();
-				
-			}
-		});
-		mnDatei.add(mntmSpeichernUnter);
-		
-		JMenu mnStammdaten = new JMenu("Stammdaten");
-		menuBar.add(mnStammdaten);
-		
-		JMenuItem mntmNachlassstunden = new JMenuItem("Nachlassstunden");
-		mnStammdaten.add(mntmNachlassstunden);
-		
-		JMenuItem mntmLehrer = new JMenuItem("Lehrer");
-		mnStammdaten.add(mntmLehrer);
-		
-		JMenuItem mntmRume = new JMenuItem("RÃ¤ume");
-		mnStammdaten.add(mntmRume);
-		
-		JMenuItem mntmKlassen = new JMenuItem("Klassen");
-		mnStammdaten.add(mntmKlassen);
-		
-		JMenu mnHilfe = new JMenu("Hilfe");
-		menuBar.add(mnHilfe);
-		
-		JMenuItem mntmSkript = new JMenuItem("Skript");
-		mnHilfe.add(mntmSkript);
-		
-		
-		mntmImport.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				clickImport();			
-			}
-		});
-		
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-		
-		final JPanel panelStatus = new JPanel();
-		contentPane.add(panelStatus, BorderLayout.SOUTH);
-	
 	}
 
-	
-
 	protected void clickSaveAs() {
-		
-		
+
 	}
 
 	protected void clickImport() {
-		//DB öffnen
+		// DB öffnen
 		File fileDB = MyFiles.openFile(this);
-		
-		//Dialog save as anbieten
+
+		// Dialog save as anbieten
 		mntmSpeichernUnter.setEnabled(true);
-		
-		//Verbindung zur Datenbank aufbauen
+
+		// Verbindung zur Datenbank aufbauen
 		String path = fileDB.getAbsolutePath();
 		Connection con = DBConnect.getConnection(path);
-		
-		//Testausgabe
+
+		// Testausgabe
 		DBConnect.listTables();
 		DBConnect.showTable("Freund");
-		
-		
-	
+
 	}
 }
