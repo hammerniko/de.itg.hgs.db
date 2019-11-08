a0241c3555e86b95854d6a915b74b844464c08af
n.hammer <n.hammer@v212-be.hgssingen.de>
2019-11-08 14:15:27 +0100

    imports not needed deleted

--- DB/src/a02_DBViewer/IOFiles.java
+++ DB/src/a02_DBViewer/IOFiles.java
@@ -1,9 +1,6 @@
 package a02_DBViewer;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
